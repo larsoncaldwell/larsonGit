ccdf640e81466df937fc62b0e78ac2465d1c7675
larsoncaldwell <cal09016@byui.edu>
2014-02-12 20:18:11 -0700

    lame more

--- Wonderfile.java
+++
@@ -1,7 +0,0 @@
-public class Wonderfile
-{
-    public static void main(String[] args)
-    {
-        System.out.println("WONDER one and all!!");
-    }
-}
---
+++ WonderfileXP.java
@@ -0,0 +1,7 @@
+public class WonderfileXP
+{
+    public static void main(String[] args)
+    {
+	System.out.println("I'm BETTER");
+    }
+}
